e23d1e39e7416f23352351d646985609767b9937
antony <anousakis.antonis@gmail.com>
2021-10-13 14:43:34 +0300

    first commit

--- test.sh
+++ test.sh
@@ -24,6 +24,17 @@ echo "password = $password"
   --compressed \
   --insecure
   
+<html>
+<head>
+	<title>Oxygen Configuration</title>
+	<meta http-equiv="Pragma" content="no-cache">
+	<meta http-equiv="Cache-Control" content="no-store, no-cache, must-revalidate">
+	<meta http-equiv="Expires" content="-1">
+	<meta http-equiv="Refresh" content="0;url=./user/page.pl">
+</head>
+<body>
+</body>
+</html>
 
 
 curl 'http://192.168.1.254/user/page.pl' \
@@ -39,6 +50,19 @@ curl 'http://192.168.1.254/user/page.pl' \
   --insecure
   
   
+  <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+	<meta http-equiv="Pragma" content="no-cache">
+	<meta http-equiv="Cache-Control" content="no-store, no-cache, must-revalidate">
+	<meta http-equiv="Expires" content="-1">
+	<meta http-equiv="Refresh" content=0;url=/cgi-bin/>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+	Please <a href='/cgi-bin/'>click here</a> if your browser does not redirect you automatically
+</body>
+</html>%  
 
 curl -u user:user 'http://192.168.1.254/cgi-bin/' \
   -H 'Connection: keep-alive' \
@@ -52,6 +76,18 @@ curl -u user:user 'http://192.168.1.254/cgi-bin/' \
   --compressed \
   --insecure
   
+  <html>
+<head>
+	<title>Oxygen Configuration</title>
+	<meta http-equiv="Pragma" content="no-cache">
+	<meta http-equiv="Cache-Control" content="no-store, no-cache, must-revalidate">
+	<meta http-equiv="Expires" content="-1">
+	<meta http-equiv="Refresh" content="0;url=./page.pl">
+</head>
+<body>
+</body>
+</html>
+
 
 
 curl -c cookies.txt 'http://192.168.1.254/cgi-bin/page.pl' \
